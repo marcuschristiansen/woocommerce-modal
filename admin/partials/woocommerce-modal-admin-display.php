6184aefad1acd3aaff88d796f46486140514577d
Marcus Christiansen <marcus@firingsquad.co.za>
2015-10-13 16:21:26 +0200

    Initial commit

---
+++ admin/partials/woocommerce-modal-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       http://xiansen.co.za
+ * @since      1.0.0
+ *
+ * @package    Woocommerce_Modal
+ * @subpackage Woocommerce_Modal/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ includes/class-woocommerce-modal-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://xiansen.co.za
+ * @since      1.0.0
+ *
+ * @package    Woocommerce_Modal
+ * @subpackage Woocommerce_Modal/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Woocommerce_Modal
+ * @subpackage Woocommerce_Modal/includes
+ * @author     Marcus Christiansen <christiansen.marcus@gmail.com>
+ */
+class Woocommerce_Modal_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
